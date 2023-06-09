a64cc7b982dd45de150cba733c5757084fb2eb41
0fatal <2816813070@qq.com>
2023-06-09 14:00:41 +0000

    feat(server): add resource usage capture and limit

--- server/src/billing/billing.module.ts
+++ server/src/billing/billing.module.ts
@@ -5,11 +5,20 @@ import { BillingController } from './billing.controller'
 import { BillingTaskService } from './billing-task.service'
 import { ApplicationModule } from 'src/application/application.module'
 import { ResourceController } from './resource.controller'
+import { ResourceTaskService } from './resource-task.service'
+import { DatabaseModule } from 'src/database/database.module'
+import { RegionModule } from 'src/region/region.module'
+import { StorageModule } from 'src/storage/storage.module'
 
 @Module({
-  imports: [ApplicationModule],
+  imports: [ApplicationModule, DatabaseModule, RegionModule, StorageModule],
   controllers: [BillingController, ResourceController],
-  providers: [BillingService, ResourceService, BillingTaskService],
+  providers: [
+    BillingService,
+    ResourceService,
+    BillingTaskService,
+    ResourceTaskService,
+  ],
   exports: [BillingService, ResourceService],
 })
 export class BillingModule {}
